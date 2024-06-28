fb9da80dd915afff72c9567235dc25fa55e7a711
Aleksandr <asimfali@mail.ru>
2024-06-28 10:19:59 +0300

    base template

--- prod/views.py
+++ prod/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+
+def home(request):
+    categories = Category.objects.filter(parent=None)
+    products = Product.objects.all()
+    return render(request, 'home.html', {'categories': categories, 'products': products})
--- tepl/urls.py
+++ tepl/urls.py
@@ -10,12 +10,19 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls.py import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls.py'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+app_name = 'shop'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('prod.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
